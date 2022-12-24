61b599ab59dae49ea272de7f29ff1a585accf221
prabalbhandary <prabalbhandary97@gmail.com>
2022-12-24 11:12:04 +0545

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,165 @@
+let allMusic = 
+[
+    {
+      name: "O Mere Sona Re",
+      artist: "Mohammad Rafi & Asha Bhosle",
+      img: "1",
+      src: "1"
+    },
+    {
+      name: "Kesariya",
+      artist: "Arijit Singh",
+      img: "2",
+      src: "2"
+    },
+    {
+      name: "Mehebooba",
+      artist: "Ananya Bhat",
+      img: "3",
+      src: "3"
+    },
+    {
+      name: "O Mere Dil K Chain",
+      artist: "Kishor Kumar",
+      img: "4",
+      src: "4"
+    },
+    {
+      name: "Rishta Tha",
+      artist: "Darshan Raval",
+      img: "5",
+      src: "5"
+    },
+    {
+      name: "Dil Ibadat",
+      artist: "KK",
+      img: "6",
+      src: "6"
+    },
+    {
+      name: "Saami Saami",
+      artist:"Mounika Yadav",
+      img: "7",
+      src: "7"
+    },
+    {
+      name: "Sri Valli",
+      artist:"Javed Ali",
+      img: "8",
+      src: "8"
+    },
+    {
+      name: "Har Har Shambhu Shiv Mahadeva",
+      artist:"Abhilipsa Panda & Jeetu Sharma",
+      img: "9",
+      src: "9"
+    },
+    {
+      name: "Pyaar Deewana Hota Hai",
+      artist:"R.D Burman",
+      img: "10",
+      src: "10"
+    },
+    {
+      name: "Sathi",
+      artist:"Sushant K.C",
+      img: "11",
+      src: "11"
+    },
+    {
+      name: "Saathi Mere Sun Toh Jara",
+      artist: "Kumar Sanu & Alka Yakin",
+      img: "12",
+      src: "12"
+    },
+    {
+      name: "Mere Haath Mein Tera Haath",
+      artist: "Jatin-Lalit, Sunidhi Chauhan & Sonu Nigam",
+      img: "13",
+      src: "13"
+    },
+    {
+      name: "Sultan",
+      artist: "K.G.F Chapter-2",
+      img: "14",
+      src: "14"
+    },
+    {
+      name: "Ishq Mein Marjawa",
+      artist: "Fanna",
+      img: "15",
+      src: "15"
+    },
+    {
+      name: "Junoon Aisi Nafarat Toh Kesa Ishq",
+      artist: "Junoon",
+      img: "16",
+      src: "16"
+    },
+    {
+      name: "Best of Kumar Sanu",
+      artist: "Kumar Sanu",
+      img: "17",
+      src: "17"
+    },
+    {
+      name: "Best of K.K",
+      artist: "K.K",
+      img: "18",
+      src: "18"
+    },
+    {
+      name: "Sanam Teri Kasam",
+      artist: "Sanam Teri Kasam",
+      img: "19",
+      src: "19"
+    },
+    {
+      name: "Nepali Old Songs Collection",
+      artist: "Nepali Old Songs",
+      img: "20",
+      src: "20"
+    },
+    {
+      name: "Best of Arijit Singh",
+      artist: "Arijit Singh",
+      img: "21",
+      src: "21"
+    },
+    {
+      name: "Monster Song",
+      artist: "K.G.F Chapter-2",
+      img: "22",
+      src: "22"
+    },
+    {
+      name: "Mera Dil Bhi",
+      artist: "Saajan",
+      img: "23",
+      src: "23"
+    },
+    {
+      name: "Tujhse Naraz Nahi Zindagi",
+      artist: "Lata Mangeshkar and Gulzar",
+      img: "24",
+      src: "24"
+    },
+    {
+      name: "Juda Hoke Bhi",
+      artist: "Atif Aslam",
+      img: "25",
+      src: "25"
+    },
+    {
+      name: "Prem Geet 3 Full Album",
+      artist: "Prem Geet 3",
+      img: "26",
+      src: "26"
+    },
+    {
+      name: "Ath Shree Mahabharat Katha",
+      artist: "Mahabharat",
+      img: "27",
+      src: "27"
+    }
+  ];
